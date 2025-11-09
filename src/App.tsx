2792869a4bbaf2fd4f6200c6b8b1538138fa50d3
Jonathan Schwarzhaupt <jonathan.schwarzhaupt@ommax.de>
2025-11-09 11:27:31 +0000

    have example app running

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { RoomShell } from "@sqlrooms/room-shell";
 import { roomStore } from "./store";
-import { ThemeProvider } from "@sqlrooms/ui";
+import { ThemeProvider, ThemeSwitch } from "@sqlrooms/ui";
 
 export const App = () => {
     return (
         <ThemeProvider defaultTheme="light" storageKey="ui-theme">
             <RoomShell className="h-screen" roomStore={roomStore}>
-                <RoomShell.Sidebar />
+                <RoomShell.Sidebar>
+                    <ThemeSwitch />
+                </RoomShell.Sidebar>
                 <RoomShell.LayoutComposer />
                 <RoomShell.LoadingProgress />
             </RoomShell>
